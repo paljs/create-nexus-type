10dd26216a59685b74c65560b78bfd6adbd9c566
Ahmed Elywa <ahmed.elywa@icloud.com>
2019-12-01 16:14:33 +0200

    formate code

--- src/types.js
+++ src/types.js
@@ -16,14 +16,11 @@ function getArgs() {
 
 function cli() {
   const args = getArgs();
-  fs.readFile(args["--schema"], { encoding: 'utf-8' }, function(
-    err,
-    data
-  ) {
+  fs.readFile(args['--schema'], { encoding: 'utf-8' }, function(err, data) {
     if (!err) {
       let fileContent = '';
       let inModel = false;
-      const dir = args["--outDir"] + "/";
+      const dir = args['--outDir'] + '/';
       const lines = data.split(`
 `);
       lines.map(line => {
@@ -31,25 +28,34 @@ function cli() {
           const lineArray = line.split(' ');
           const filteredArray = lineArray.filter(v => v);
           if (['model', 'enum'].includes(filteredArray[0])) {
-            fileContent += fileContent !== ""? `
-            
-`: "";
-            fileContent += filteredArray[0] === 'model' ? `export interface ${filteredArray[1]} {`: `enum ${filteredArray[1]} {`;
+            fileContent +=
+              fileContent !== ''
+                ? `
+
+`
+                : '';
+            fileContent +=
+              filteredArray[0] === 'model'
+                ? `export interface ${filteredArray[1]} {`
+                : `enum ${filteredArray[1]} {`;
             inModel = filteredArray[0];
           } else if (inModel) {
             if (filteredArray[0] !== '}') {
-                if(inModel === 'enum'){
-                    fileContent += `
+              if (inModel === 'enum') {
+                fileContent += `
   ${filteredArray[0]} = '${filteredArray[0]}',`;
-                }else {
-                    const type = types[filteredArray[1].replace('?', '')] ? types[filteredArray[1].replace('?', '')] : filteredArray[1].replace('?', '');
-                    fileContent += `
-  ${filteredArray[0]}?: ${type};`;
-                }
+              } else {
+                const type = types[filteredArray[1].replace('?', '')]
+                  ? types[filteredArray[1].replace('?', '')] +
+                    (filteredArray[1].includes('?') ? ' | null' : '')
+                  : filteredArray[1].replace('?', ' | null');
+                fileContent += `
+  ${filteredArray[0]}: ${type};`;
+              }
             } else {
               fileContent += `
 }`;
-    inModel = false
+              inModel = false;
             }
           }
         }
@@ -65,14 +71,14 @@ function cli() {
   });
 }
 const types = {
-    Int: 'number',
-    Float: 'number',
-    String: 'string',
-    Boolean: 'boolean',
-    "Int[]": 'number[]',
-    "Float[]": 'number[]',
-    "String[]": 'string[]',
-    "Boolean[]": 'boolean[]',
-    DateTime: 'Date',
-}
+  Int: 'number',
+  Float: 'number',
+  String: 'string',
+  Boolean: 'boolean',
+  'Int[]': 'number[]',
+  'Float[]': 'number[]',
+  'String[]': 'string[]',
+  'Boolean[]': 'boolean[]',
+  DateTime: 'Date'
+};
 cli();
